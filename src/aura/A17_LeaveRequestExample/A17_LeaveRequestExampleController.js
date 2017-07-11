b01eda78f8fd64ff4402226e08863deb203331b0
Sebastian Kubalski <s.kubalski@polsource.com>
2017-07-11 09:23:47 +0200

    — update

--- src/aura/A17_LeaveRequestExample/A17_LeaveRequestExampleController.js
+++ src/aura/A17_LeaveRequestExample/A17_LeaveRequestExampleController.js
@@ -3,52 +3,66 @@
  */
 ({
     doGetAllLeaveRequests: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getAllLeaveRequests');
     },
     doSearchLeaveRequest: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.searchLeaveRequest');
     },
     doGetLeaveRequestsByUser: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getLeaveRequestsByUser', {
-            userId: event.getParam('v.userId')
+            userId: event.getParam('userId')
         });
     },
     doGetLeaveRequestsByCurrentUser: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getLeaveRequestsByCurrentUser');
     },
     doGetLeaveRequestByType: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getLeaveRequestByType');
     },
     doGetHerokuData: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getHerokuData');
     },
     doGetLeaveRequestBetweenDates: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getLeaveRequestBetweenDates', {
-            startDate: event.getParam('v.startDate'),
-            endDate: event.getParam('v.endDate')
+            startDate: event.getParam('startDate'),
+            endDate: event.getParam('endDate')
         });
     },
     doGetLeaveRequestsGroupedByDate: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getLeaveRequestsGroupedByDate');
     },
     doGetLeaveRequestsGroupedByUser: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getLeaveRequestsGroupedByUser');
     },
     doGetUsersByRemainingDays: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getUsersByRemainingDays', {
             numberOfDays: event.getParam('numberOfDays')
         });
     },
     doGetApprovalHistories: function (component, event, helper) {
+        component.set('v.results', '');
         helper.doRequest(component, 'c.getApprovalHistories');
     },
     processLeaveRequestsByUser: function (component, event, helper) {
+        component.set('v.results', '');
         helper.processSecondStep(component, 'c:A17_LeaveRequestsByUserExample');
     },
     processLeaveRequestBetweenDates: function (component, event, helper) {
+        component.set('v.results', '');
         helper.processSecondStep(component, 'c:A17_LeaveRequestBetweenDatesExample');
     },
     processUsersByRemainingDays: function (component, event, helper) {
+        component.set('v.results', '');
         helper.processSecondStep(component, 'c:A17_UsersByRemainingDaysExample');
     },
     closeSecondStep: function (component, event, helper) {
